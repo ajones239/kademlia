9adde1a4026509672ecae0c4caee2474424b3e31
Aidan Jones <arjones239@protonmail.ch>
2022-05-08 15:42:48 -0700

    fixed rpc library path bug

--- src/peer1.c
+++ src/peer1.c
@@ -16,39 +16,10 @@ int main(int argc, char **argv) {
     sleep(1);
 
 
-    kademlia_ping_t var;
-
-    var.id = self->self.id;
-    var.host = self->self.host;
-    var.proto = IPPROTO_TCP;
-
-    CLIENT *clnt;
-    clnt = clnt_create("127.0.0.1", MESSAGE_PROG, MESSAGE_VERS, "tcp");
-    if (clnt == NULL) {
-        fprintf(stderr, "error creating rpc client\n");
-        return 1;
-    }
-    int *r;
-    r = kademlia_ping_1(&var, clnt);
-    if (r == NULL) {
-        fprintf(stderr, "error pinging");
-        return 1;
-    } else printf("%d\n", *r);
-    free(var.host);
-    free(clnt);
 
 
-
-
-
-
-
-    if (kademlia_send_ping("127.0.0.1") == -1) {
+    if (kademlia_network_bootstrap("127.0.0.1") == -1) {
         fprintf(stderr, "failed pinging remote host");
-        return -1;
-    }
-    if (kademlia_send_find_node(self->self.id, "127.0.0.1") == -1) {
-        fprintf(stderr, "failed getting peers from remote host");
         return -1;
     }
 
